7fb5771b06dc10f71bb2642fcdc5c07f0e7e20cf
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-07-08 14:21:40 +0200

    add example social links

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
-//
+
+const socialsMobile = {
+  youtube: 'm.youtube/c/{PROFILE_ID}',
+  twitch: 'm.twitch.tv/{PROFILE_ID}',
+  facebook: 'm.facebook.com/{PROFILE_ID}',
+  linkedin: 'linkedin.com/mwlite/in/{PROFILE_ID}',
+};
+
+const socialsDesktop = {
+  linkedin: '(www.)?linkedin.com/in/{PROFILE_ID}',
+  twitter: '(www.)?twitter.com/{PROFILE_ID}',
+  facebook: '(www.)?facebook.com/{PROFILE_ID}',
+  youtube: '(www.)youtube.com/channel/{PROFILE_ID}',
+  twitch: '(www.)?twitch.tv/{PROFILE_ID}',
+  discord: 'link',
+  instagram: '(www.)?instagram.com/{PROFILE_ID}',
+  patreon: '(www.)?patreon.com/{PROFILE_ID}',
+};
+
+console.log(socialsDesktop);
