6ca2d66ce096a107ad092a030ef5cd03d02bdd56
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-08-02 21:52:51 +0200

    fix imports in index.ts file

--- src/profiles/index.ts
+++ src/profiles/index.ts
@@ -1,13 +1,29 @@
 
 import { Profile } from '../main';
 
-const fs = require('fs');
-const files = fs.readdirSync(__dirname)
-  .filter((name: string) => !name.endsWith('.spec.ts'))
-  .filter((name: string) => name !== 'index.ts');
+import { behance } from './behance';
+import { dribble } from './dribble';
+import { facebook } from './facebook';
+import { github } from './github';
+import { instagram } from './instagram';
+import { linkedin } from './linkedin';
+import { medium } from './medium';
+import { patreon } from './patreon';
+import { twitch } from './twitch';
+import { twitter } from './twitter';
+import { youtube } from './youtube';
 
-export const PREDEFINED_PROFILES: Profile[] = [];
-files.map((file: string) => {
-  const [ profile ] = Object.values(require([__dirname, file].join('/'))) as Profile[];
-  PREDEFINED_PROFILES.push(profile);
-});
+
+export const PREDEFINED_PROFILES: Profile[] = [
+  behance,
+  dribble,
+  facebook,
+  github,
+  instagram,
+  linkedin,
+  medium,
+  patreon,
+  twitch,
+  twitter,
+  youtube,
+];
