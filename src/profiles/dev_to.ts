eedc3e2b994ddc10c70459cf2709ab8a273fabf8
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-09-03 22:42:16 +0200

    fix: dev.to regexp

--- src/profiles/dev_to.ts
+++ src/profiles/dev_to.ts
@@ -5,7 +5,7 @@ export const dev_to = {
   name: 'dev_to',
   matches: [
     {
-      match: '(https?://)?(www.)?dev.to/?({PROFILE_ID})/?', group: 3, type: TYPE_DESKTOP,
+      match: '(https?://)?(www.)?dev.to/({PROFILE_ID})/?', group: 3, type: TYPE_DESKTOP,
       pattern: 'https://dev.to/{PROFILE_ID}'
     },
     { match: '({PROFILE_ID})', group: 1 },
