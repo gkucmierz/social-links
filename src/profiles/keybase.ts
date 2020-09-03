8be86ccad1cc17c97ece00a02e100f4cedc22b18
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-09-03 22:41:48 +0200

    fix: keybase regexp

--- src/profiles/keybase.ts
+++ src/profiles/keybase.ts
@@ -5,7 +5,7 @@ export const keybase = {
   name: 'keybase',
   matches: [
     {
-      match: '(https?://)?(www.)?keybase.io/?({PROFILE_ID})/?', group: 3, type: TYPE_DESKTOP,
+      match: '(https?://)?(www.)?keybase.io/({PROFILE_ID})/?', group: 3, type: TYPE_DESKTOP,
       pattern: 'https://keybase.io/{PROFILE_ID}'
     },
     { match: '({PROFILE_ID})', group: 1 },
