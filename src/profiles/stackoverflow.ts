c1fabe90febf512c1a8291456fc684bc0c83c8fa
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-09-03 22:40:38 +0200

    fix: stackoverflow regex

--- src/profiles/stackoverflow.ts
+++ src/profiles/stackoverflow.ts
@@ -5,7 +5,7 @@ export const stackoverflow = {
   name: 'stackoverflow',
   matches: [
     {
-      match: '(https?://)?(www.)?stackoverflow.com/users/?({PROFILE_ID})(/[A-Za-z0-9_\\-\\.]+)?/?', group: 3, type: TYPE_DESKTOP,
+      match: '(https?://)?(www.)?stackoverflow.com/users/({PROFILE_ID})(/[A-Za-z0-9_\\-\\.]+)?/?', group: 3, type: TYPE_DESKTOP,
       pattern: 'https://stackoverflow.com/users/{PROFILE_ID}'
     },
     { match: '({PROFILE_ID})', group: 1 },
