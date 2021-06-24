fa149df4c1a7ef7d9e55b1324b333b59ca435d2c
Nicolás Font <nicolasfont@gmail.com>
2021-06-24 10:03:18 -0700

    add profile: pinterest

---
+++ src/profiles/pinterest.ts
@@ -0,0 +1,13 @@
+
+import { TYPE_DESKTOP, TYPE_MOBILE } from '../types';
+
+export const pinterest = {
+  name: 'pinterest',
+  matches: [
+    {
+      match: '(https?://)?(www.)?pinterest.com/({PROFILE_ID})/?', group: 3, type: TYPE_DESKTOP,
+      pattern: 'https://pinterest.com/{PROFILE_ID}'
+    },
+    { match: '@?({PROFILE_ID})', group: 1 },
+  ]
+};
